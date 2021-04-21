171d7c6ad7be6acc01515526c76cc85e0ea9b081
Buckley212 <rob36611@gmail.com>
2021-04-21 16:00:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  const firstFive = contacts.slice(0, 5);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+        <tr>
+          <td>Picture</td>
+          <td>Name</td>
+          <td>Populariry</td>
+        </tr>
+        </thead>
+        <tbody>
+          {firstFive.map(eachContact => {
+            return (
+              <tr>
+                <td><img src={eachContact.pictureUrl}></img></td>
+                <td>{eachContact.name}</td>
+                <td>{eachContact.popularity}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
