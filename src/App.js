2cf273e5421d16399c17467b89323a360df2b6ab
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-04-21 16:35:07 -0400

    iteration 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import contacts from "./contacts.json";
+import { useState } from 'react'
 
 function App() {
+
   const firstFive = contacts.slice(0, 5);
+  const [showContacts, setShowContacts] = useState(firstFive)
+  const [contact, setContact] = useState(0)
+  const getRandomContact = () => {
+    console.log("getting random celeb")
+    let contactIndex = Math.floor(Math.random() * contacts.length)
+    console.log(contactIndex)
+    console.log(contacts[contactIndex])
+    // showContacts.push(contacts[contactIndex])
+    // setShowContacts([...showContacts, contacts[contactIndex]])
+    let copyContacts = [...showContacts]
+    copyContacts.push(contacts[contactIndex])
+    setShowContacts(copyContacts)
+  }
+
   return (
+
     <div className="App">
+    <button onClick={getRandomContact}>Add Random Contact</button>
       <table>
         <thead>
         <tr>
@@ -14,12 +32,13 @@ function App() {
         </tr>
         </thead>
         <tbody>
-          {firstFive.map(eachContact => {
+          {showContacts.map(eachContact => {
             return (
               <tr>
                 <td><img src={eachContact.pictureUrl}></img></td>
                 <td>{eachContact.name}</td>
                 <td>{eachContact.popularity}</td>
+      
               </tr>
             )
           })}
